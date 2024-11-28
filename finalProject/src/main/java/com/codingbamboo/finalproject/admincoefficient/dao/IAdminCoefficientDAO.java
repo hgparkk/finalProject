9b4ab647eedd2a61ca2895b78112983425e79dd7
kkj060 <kkj060@naver.com>
2024-11-28 18:02:23 +0900

    김기주 코드 수정

--- finalProject/src/main/java/com/codingbamboo/finalproject/admincoefficient/dao/IAdminCoefficientDAO.java
+++ finalProject/src/main/java/com/codingbamboo/finalproject/admincoefficient/dao/IAdminCoefficientDAO.java
@@ -1,49 +1,24 @@
 package com.codingbamboo.finalproject.admincoefficient.dao;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.Map;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.HashMap;
+
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
 
 import com.codingbamboo.finalproject.admincoefficient.dto.AdminCoefficientDTO;
+import com.codingbamboo.finalproject.coefficient.dto.CoefficientDTO;
 
+@Mapper
 public interface IAdminCoefficientDAO {
-    private Connection conn;
+	// 저장
+	int insertCf(AdminCoefficientDTO adminCoefficientDTO);
+	
+	// 업데이트
+	int updateCf(AdminCoefficientDTO coefficient);
+	
+	// 불러오기
+	List<CoefficientDTO> getCoefficientValue();
+	
 
-    public IAdminCoefficientDAO(Connection conn) {
-        this.conn = conn;
-    }
-
-    // 특정 계수 조회 (모든 계수 가져오기)
-    public Map<Integer, AdminCoefficientDTO> getAllCoefficients() throws SQLException {
-        String sql = "SELECT coefficient_no, coefficient_name, coefficient_value FROM coefficient";
-        Map<Integer, AdminCoefficientDTO> coefficients = new HashMap<>();
-
-        try (PreparedStatement pstmt = conn.prepareStatement(sql);
-             ResultSet rs = pstmt.executeQuery()) {
-            while (rs.next()) {
-            	AdminCoefficientDTO coefficient = new AdminCoefficientDTO(
-                        rs.getInt("coefficient_no"),
-                        rs.getString("coefficient_name"),
-                        rs.getDouble("coefficient_value")
-                );
-                coefficients.put(coefficient.getCoefficientNo(), coefficient);
-            }
-        }
-        return coefficients;
-    }
-
-    // 계수 값 수정
-    public void updateCoefficient(int coefficientNo, double coefficientValue) throws SQLException {
-        String sql = "UPDATE coefficient SET coefficient_value = ? WHERE coefficient_no = ?";
-
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setDouble(1, coefficientValue);
-            pstmt.setInt(2, coefficientNo);
-            pstmt.executeUpdate();
-        }
-    }
 }
--- finalProject/src/main/java/com/codingbamboo/finalproject/admincoefficient/service/AdminCoefficientService.java
+++ finalProject/src/main/java/com/codingbamboo/finalproject/admincoefficient/service/AdminCoefficientService.java
@@ -1,28 +1,21 @@
 package com.codingbamboo.finalproject.admincoefficient.service;
 
-import java.sql.SQLException;
-import java.util.Map;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.codingbamboo.finalproject.admincoefficient.dao.IAdminCoefficientDAO;
-import com.codingbamboo.finalproject.admincoefficient.dto.AdminCoefficientDTO;
+import com.codingbamboo.finalproject.coefficient.dto.CoefficientDTO;
 
+@Service
 public class AdminCoefficientService {
-    private IAdminCoefficientDAO dao;
 
-    public AdminCoefficientService(IAdminCoefficientDAO coefficientDAO) {
-        this.dao = coefficientDAO;
-    }
-
-    // 전체 계수 조회
-    public Map<Integer, AdminCoefficientDTO> getAllCoefficients() throws SQLException {
-        return dao.getAllCoefficients();
-    }
-
-    // 계수 수정
-    public void updateCoefficient(int coefficientNo, double coefficientValue) throws IllegalArgumentException, SQLException {
-        if (coefficientValue < 0) {
-            throw new IllegalArgumentException("계수 값은 0 이상이어야 합니다.");
-        }
-        dao.updateCoefficient(coefficientNo, coefficientValue);
-    }
+	@Autowired
+	IAdminCoefficientDAO dao;
+ 	
+	public List<CoefficientDTO> getCoefficientValue(){
+ 		return dao.getCoefficientValue();
+ 	}
+	
 }
