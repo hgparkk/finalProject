060db57ae567666c958b4953a2aa5fc5ca075b27
kkj060 <kkj060@naver.com>
2024-11-28 17:47:21 +0900

    김기주 코드병합

--- finalProject/src/main/java/com/codingbamboo/finalproject/admincoefficient/dto/AdminCoefficientDTO.java
+++ finalProject/src/main/java/com/codingbamboo/finalproject/admincoefficient/dto/AdminCoefficientDTO.java
@@ -1,5 +1,51 @@
 package com.codingbamboo.finalproject.admincoefficient.dto;
 
 public class AdminCoefficientDTO {
+    private int coefficientNo;        // 계수 번호
+    private String coefficientName;   // 계수 이름
+    private double coefficientValue;  // 계수 값
+	
+    public AdminCoefficientDTO() {
+		super();
+	}
 
+	public AdminCoefficientDTO(int coefficientNo, String coefficientName, double coefficientValue) {
+		super();
+		this.coefficientNo = coefficientNo;
+		this.coefficientName = coefficientName;
+		this.coefficientValue = coefficientValue;
+	}
+
+	@Override
+	public String toString() {
+		return "AdminCoefficientDTO [coefficientNo=" + coefficientNo + ", coefficientName=" + coefficientName
+				+ ", coefficientValue=" + coefficientValue + "]";
+	}
+
+	public int getCoefficientNo() {
+		return coefficientNo;
+	}
+
+	public void setCoefficientNo(int coefficientNo) {
+		this.coefficientNo = coefficientNo;
+	}
+
+	public String getCoefficientName() {
+		return coefficientName;
+	}
+
+	public void setCoefficientName(String coefficientName) {
+		this.coefficientName = coefficientName;
+	}
+
+	public double getCoefficientValue() {
+		return coefficientValue;
+	}
+
+	public void setCoefficientValue(double coefficientValue) {
+		this.coefficientValue = coefficientValue;
+	}
+    
+    
+    
 }
--- finalProject/src/main/java/com/codingbamboo/finalproject/admincoefficient/service/AdminCoefficientService.java
+++ finalProject/src/main/java/com/codingbamboo/finalproject/admincoefficient/service/AdminCoefficientService.java
@@ -1,5 +1,28 @@
 package com.codingbamboo.finalproject.admincoefficient.service;
 
-public class AdminCoefficientService {
+import java.sql.SQLException;
+import java.util.Map;
 
+import com.codingbamboo.finalproject.admincoefficient.dao.IAdminCoefficientDAO;
+import com.codingbamboo.finalproject.admincoefficient.dto.AdminCoefficientDTO;
+
+public class AdminCoefficientService {
+    private IAdminCoefficientDAO dao;
+
+    public AdminCoefficientService(IAdminCoefficientDAO coefficientDAO) {
+        this.dao = coefficientDAO;
+    }
+
+    // 전체 계수 조회
+    public Map<Integer, AdminCoefficientDTO> getAllCoefficients() throws SQLException {
+        return dao.getAllCoefficients();
+    }
+
+    // 계수 수정
+    public void updateCoefficient(int coefficientNo, double coefficientValue) throws IllegalArgumentException, SQLException {
+        if (coefficientValue < 0) {
+            throw new IllegalArgumentException("계수 값은 0 이상이어야 합니다.");
+        }
+        dao.updateCoefficient(coefficientNo, coefficientValue);
+    }
 }
